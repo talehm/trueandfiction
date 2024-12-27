bd209951e9675c82cf4b16d46494ca6835873c12
talehm <talehmuzaffer@gmail.com>
2024-12-27 21:52:37 +0100

    build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,18 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	entry: {
-		app: path.resolve(__dirname, 'src/app.js'), // Entry for your main app
-	},
+	entry: path.resolve(__dirname, 'src/app.js'),
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-	  filename: '[name].[contenthash].js', // Use contenthash for better caching
-	  chunkFilename: '[name].[contenthash].js', // File name for lazy-loaded chunks
+	  filename: 'vue-wordpress.js'
   },
   module: {
 	  rules: [
@@ -23,26 +19,36 @@ module.exports = {
 					  loader: 'file-loader',
 					  options: {
 						  name: '[name].[ext]', // Preserve original file name and extension
-						  outputPath: 'fonts/', // Output directory for fonts
+						  outputPath: 'fonts/',  // Output directory for fonts
 					  },
 				  },
 			  ],
 		  },
 		  {
 			  test: /\.less$/,
-			  use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'], // Using MiniCssExtractPlugin for CSS extraction
+			  use: [
+				  'style-loader',
+				  'css-loader',
+				  'less-loader'
+			  ]
 		  },
       {
         test: /\.s(c|a)ss$/,
 		  use: [
-			  MiniCssExtractPlugin.loader, // Use MiniCssExtractPlugin for extracting CSS
+			  'vue-style-loader',
 			  'css-loader',
 			  {
 				  loader: 'sass-loader',
+				  // Requires sass-loader@^7.0.0
+				  options: {
+					  implementation: require('sass'),
+					  indentedSyntax: true // optional
+				  },
+				  // Requires >= sass-loader@^8.0.0
 				  options: {
 					  implementation: require('sass'),
 					  sassOptions: {
-						  indentedSyntax: true, // optional
+						  indentedSyntax: true // optional
 					  },
 				  },
 			  },
@@ -50,55 +56,32 @@ module.exports = {
       },
       {
         test: /\.vue$/,
-		  loader: 'vue-loader',
+		  loader: 'vue-loader'
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-		  exclude: /node_modules/,
+		  exclude: /node_modules/
       },
       {
         test: /\.css$/,
-			  use: ['style-loader', // Add style-loader or vue-style-loader for Vue
-				  'css-loader',]
-		  },
-	  ],
+			  use: [
+				  MiniCssExtractPlugin.loader,
+				  'css-loader'
+			  ]
+		  }
+	  ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-		filename: '[name].[contenthash].css', // Extracted CSS chunk with contenthash for caching
-		chunkFilename: '[id].[contenthash].css', // Extracted CSS chunk for non-entry points
-	}),
-	  new WebpackManifestPlugin({
-		  fileName: 'manifest.json', // Generate manifest.json file
-		  publicPath: '/', // Specify the public path to assets
-	  }),
+		filename: 'vue-wordpress.css'
+	})
   ],
   resolve: {
     alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-		extensions: ['*', '.js', '.vue', '.json'],
-	},
-	optimization: {
-		splitChunks: {
-			chunks: 'all', // Split both dynamic and initial chunks
-			minSize: 20000, // Only chunks over 20KB will be split
-			maxSize: 50000, // Limit chunk size to 50KB if needed
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,  // Vendor libraries in separate chunk
-					name: 'vendors',
-					chunks: 'all',
-				},
-				default: {
-					minChunks: 2,  // At least 2 chunks should share the module
-					priority: -20,
-					reuseExistingChunk: true,
-				},
-			},
-		},
-		runtimeChunk: 'single',  // Create a separate runtime chunk for better caching
-	},
-};
+		  '@': path.resolve(__dirname, './src')
+	  },
+		extensions: ['*', '.js', '.vue', '.json']
+	}
+}
