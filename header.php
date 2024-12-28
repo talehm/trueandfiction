4f2f6760b2547bd38b5dd6563084488ff65bf8d7
talehm <talehmuzaffer@gmail.com>
2024-12-28 18:06:46 +0100

    google tag

--- header.php
+++ header.php
@@ -9,6 +9,15 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5TPXBQZW');</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9Q7E5BR7G6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9Q7E5BR7G6');
+</script>
 <!-- End Google Tag Manager -->
     <meta charset="<?php bloginfo( 'charset' ); ?>">
 
